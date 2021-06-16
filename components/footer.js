89d6cd235a2ebc6fd039405bcf976bc55e5c7aa7
reizist <reizist@gmail.com>
2021-06-16 15:46:30 +0900

    Add unquete link to footer

--- components/footer.js
+++ components/footer.js
@@ -9,16 +9,34 @@ export default function Footer({ token }) {
   } else {
     qrCodeUrl =
       "https://chart.apis.google.com/chart?cht=qr&chs=150x150&chl=" +
-      "https://recorder-delta.vercel.app/drill?token=" +
+      "https://recorder-reizist.vercel.app/drill?token=" +
       token;
   }
   return (
     <footer className="flex items-center justify-center border-t mt-4">
-      <img src={qrCodeUrl} />
-      Powered by{" "}
-      <span className="pl-1 text-base text-purple-700">
-        計算ドリルジェネレータ
-      </span>
+      <div className="flex-box">
+        <div>
+          <img src={qrCodeUrl} className="mt-2" />
+          Powered by{" "}
+          <span className="pl-1 text-base text-purple-700">
+            <a href="https://recorder-reizist.vercel.app/">
+              計算ドリルジェネレータ
+            </a>
+          </span>
+        </div>
+        <div className="flex-box without-print">
+          <span className="text-base">
+            改善/機能追加依頼は
+            <a
+              href="https://forms.gle/UxUsnTzAXKxYFg7q6"
+              className="text-purple-700"
+            >
+              コチラ
+            </a>
+            にお願いします。
+          </span>
+        </div>
+      </div>
     </footer>
   );
 }
