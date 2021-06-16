54b841e40150b51da6592fdd1a3dc361fb56a6c6
reizist <reizist@gmail.com>
2021-06-16 22:42:09 +0900

    Adding GA for analysis

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,22 @@
 import "../styles/globals.css";
 import Head from "next/head";
 import Layout from "../components/layout";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 
 function Recorder({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <Layout>
       <Head>
