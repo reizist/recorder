350dbffa9c178017bc1d0c500aed67eb8f0148a9
reizist <reizist@gmail.com>
2021-06-17 16:28:24 +0900

    Locate print button

--- pages/drill.js
+++ pages/drill.js
@@ -101,6 +101,13 @@ export default function Drill({ token, quizes }) {
             })}
           </div>
         ))}
+
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded without-print"
+          onClick={() => window.print()}
+        >
+          プリントする
+        </button>
       </main>
 
       <Footer token={token}></Footer>
