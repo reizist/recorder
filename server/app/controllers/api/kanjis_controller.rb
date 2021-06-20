59988eaca7a1a959521b536f64e9f558f926a128
reizist <reizist@gmail.com>
2021-06-21 03:06:25 +0900

    Considering error case

--- server/app/controllers/api/kanjis_controller.rb
+++ server/app/controllers/api/kanjis_controller.rb
@@ -1,8 +1,17 @@
 class Api::KanjisController < ApplicationController
   def meta
     codes = params["codes"].split(',')
-    metas = codes.map{|c| JSON.parse(File.read(Rails.root.join('public', 'kanji', 'meta', "#{c}.json")))}
+    results = []
+    codes.each do |code|
+      begin
+        data = JSON.parse(File.read(Rails.root.join('public', 'kanji', 'meta', "#{code}.json")))
+      rescue => e
+        next
+        puts e
+      end
+      results << data
+    end
 
-    render json: { meta: metas }
+    render json: { meta: results }
   end
 end
