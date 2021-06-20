e5a1126db6e5afb96f15c3372d49f0734748550e
reizist <reizist@gmail.com>
2021-06-21 02:20:06 +0900

    Implement meta api

--- server/app/controllers/api/kanjis_controller.rb
+++ server/app/controllers/api/kanjis_controller.rb
@@ -1,5 +1,8 @@
 class Api::KanjisController < ApplicationController
   def meta
-    render json: {hoge: "hoge"}
+    codes = params["codes"].split(',')
+    metas = codes.map{|c| JSON.parse(File.read(Rails.root.join('public', 'kanji', 'meta', "#{c}.json")))}
+
+    render json: { meta: metas }
   end
 end
