fa95bf2adb30861b8341fb9fce262f83d2683cd7
reizist <reizist@gmail.com>
2022-01-06 23:56:01 +0900

    Grid design with Grid css

--- front/pages/times_table/drill.js
+++ front/pages/times_table/drill.js
@@ -11,7 +11,59 @@ function range(end) {
 export default function Drill({ num }) {
   return (
     <>
-      <div>{range(num)}</div>
+      <style jsx media="all">{`
+        @media all {
+          .grid {
+            border: solid 1px green;
+            background-size: 1em 1em;
+            background-image: linear-gradient(
+                0deg,
+                transparent calc(1em - 1px),
+                gray 1em
+              ),
+              linear-gradient(90deg, transparent calc(1em - 1px), gray 1em);
+
+            width: 55em;
+            height: 55em;
+            display: grid;
+            grid:
+              ". . . . . . . . ." 1em
+              ". . . . . . . . ." 2em
+              ". . . . . . . . ." 3em
+              ". . . . . . . . ." 4em
+              ". . . . . . . . ." 5em
+              ". . . . . . . . ." 6em
+              ". . . . . . . . ." 7em
+              ". . . . . . . . ." 8em
+              ". . . . . . . . ." 9em
+              ". . . . . . . . ." 10em
+              / 1em 2em 3em 4em 5em 6em 7em 8em 9em 10em;
+          }
+
+          .grid > div {
+            border: solid 1px green;
+            font-size: 2em;
+
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+
+          .grid > div:nth-child(2),
+          .grid > div:nth-child(3),
+          .grid > div:nth-child(4),
+          .grid > div:nth-child(5),
+          .grid > div:nth-child(6),
+          .grid > div:nth-child(7),
+          .grid > div:nth-child(8),
+          .grid > div:nth-child(9),
+          .grid > div:nth-child(10),
+          .grid > div:nth-child(10n + 1) {
+            font-size: inherit;
+          }
+        }
+      `}</style>
+
       <div className="h-screen">
         <main className="container max-w-6xl mx-auto items-center justify-center text-center mt-8 mb-8 h-auto">
           <div className="mb-4">
@@ -24,30 +76,24 @@ export default function Drill({ num }) {
           </div>
 
           <div className="w-full mx-auto">
-            <div className="table">
-              <div className="table-row-group">
-                {range(num).map((n) => (
-                  <div className="table-row">
+            <div className="grid">
+              {range(num).map((n) => {
+                return (
+                  <>
                     {range(num).map((m) => {
                       return (
                         <div
-                          className="table-cell bg-green-100 text-align align-middle border-solid border-2 border-green-800"
                           style={{
-                            height: `${15 * n}px`,
-                            width: `${15 * m}px`,
-                            fontSize: `${
-                              m != 1 && n != 1 ? 15 + (n - 1) * 2 : 12
-                            }px`,
-                            fontFamily: "ヒラギノ角ゴ ProN W3",
+                            color: `${m == n ? "red" : ""}`,
                           }}
                         >
                           {m * n}
                         </div>
                       );
                     })}
-                  </div>
-                ))}
-              </div>
+                  </>
+                );
+              })}
             </div>
           </div>
         </main>
